11c1d0b472ce74db9e67db6ec9970f835486a86f
Nathan2128 <nathan.sinnat@gmail.com>
2024-10-29 14:26:02 -0400

    move token salt to config file and use phx to generate a better salt

--- lib/rent_a_car_web/auth_token.ex
+++ lib/rent_a_car_web/auth_token.ex
@@ -1,12 +1,10 @@
 defmodule RentACarWeb.AuthToken do
-  @token_salt "RentMeNow"
-
   @doc """
   Generates a token by encoding and signing the given `user` id,
   which clients can use for API authentication.
   """
   def sign(user) do
-    Phoenix.Token.sign(RentACarWeb.Endpoint, @token_salt, %{id: user.id})
+    Phoenix.Token.sign(RentACarWeb.Endpoint, token_salt(), %{id: user.id})
   end
 
   @doc """
@@ -14,8 +12,10 @@ defmodule RentACarWeb.AuthToken do
   ensuring its authenticity and integrity.
   """
   def verify(token) do
-    Phoenix.Token.verify(RentACarWeb.Endpoint, @token_salt, token, [
-      max_age: 365 * 24 * 3600
-    ])
+    Phoenix.Token.verify(RentACarWeb.Endpoint, token_salt(), token, max_age: 365 * 24 * 3600)
+  end
+
+  defp token_salt do
+    Application.get_env(:rent_a_car, __MODULE__)[:token_salt] |> IO.inspect()
   end
 end
